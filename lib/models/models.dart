5e07e3b537f7f951917f6b74299be09df1903f30
Anom Ayu Radhaswasti <anom@MacBook-Air-Anom.local>
2026-05-24 22:35:57 +0700

    backup code aku sebelum pull

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -30,7 +30,7 @@ class Field {
 
 class Booking {
   final String bookingCode;
-  final String username; //
+  final String username;
   final Field field;
   final String subField;
   final DateTime date;
@@ -59,72 +59,85 @@ class Booking {
 }
 
 class AppData {
-  // 👈 SIMULASI LOGIN: Ganti 'Anom' jadi 'Ayu' di sini untuk ngetes perpindahan akun!
+
+  // Simulasi Login
   static String currentUser = 'Anom';
 
   static List<Field> fields = [
+
     Field(
       id: '1',
       name: 'Telkom University Futsal',
       type: 'Vinyl Court',
       pricePerHour: 45000,
-      image: 'https://images.unsplash.com/photo-1574629810360-7efbbe195018?w=500',
+      image:
+      'https://images.unsplash.com/photo-1574629810360-7efbbe195018?w=500',
       rating: 4.9,
       location: 'Bojongsoang, Bandung',
       latitude: -6.9740,
       longitude: 107.6303,
     ),
+
     Field(
       id: '2',
       name: 'Podomoro Futsal Hub',
       type: 'Synthetic Grass',
       pricePerHour: 65000,
-      image: 'https://images.unsplash.com/photo-1529900948632-58674ba79291?w=500',
+      image:
+      'https://images.unsplash.com/photo-1529900948632-58674ba79291?w=500',
       rating: 4.7,
       location: 'Bojongsoang, Bandung',
       latitude: -6.9800,
       longitude: 107.6350,
     ),
+
     Field(
       id: '3',
       name: 'Bojongsoang Sport Center',
       type: 'Interlocking',
       pricePerHour: 55000,
-      image: 'https://images.unsplash.com/photo-1518605368461-1ee7c68154a1?w=500',
+      image:
+      'https://images.unsplash.com/photo-1518605368461-1ee7c68154a1?w=500',
       rating: 4.5,
       location: 'Bojongsoang, Bandung',
       latitude: -6.9700,
       longitude: 107.6400,
     ),
+
     Field(
       id: '4',
       name: 'Sintra Futsal Dago',
       type: 'Vinyl Court',
       pricePerHour: 50000,
-      image: 'https://images.unsplash.com/photo-1574629810360-7efbbe195018?w=500',
+      image:
+      'https://images.unsplash.com/photo-1574629810360-7efbbe195018?w=500',
       rating: 4.8,
       location: 'Dago, Bandung',
       latitude: -6.8797,
       longitude: 107.6186,
     ),
+
     Field(
       id: '5',
       name: 'Z-Futsal Antapani',
       type: 'Synthetic Grass',
       pricePerHour: 60000,
-      image: 'https://images.unsplash.com/photo-1529900948632-58674ba79291?w=500',
+      image:
+      'https://images.unsplash.com/photo-1529900948632-58674ba79291?w=500',
       rating: 4.5,
       location: 'Antapani, Bandung',
       latitude: -6.9147,
       longitude: 107.6624,
     ),
+
     Field(
       id: '6',
       name: 'Champion Arena Buah Batu',
       type: 'Interlocking',
       category: 'Outdoor',
       pricePerHour: 80000,
-      image: 'https://images.unsplash.com/photo-1518605368461-1ee7c68154a1?w=500',
+      image:
+      'https://images.unsplash.com/photo-1518605368461-1ee7c68154a1?w=500',
       rating: 4.9,
       location: 'Buah Batu, Bandung',
       latitude: -6.9475,
@@ -143,7 +156,9 @@ class AppData {
   ];
 
   static List<String> bookedSlots = [];
-  static Map<String, List<String>> bookedSlotsByDate = {};
-  static List<Booking> recentBookings = [];
 
+
+  static Map<String, List<String>> bookedSlotsByDate = {};
+
+  static List<Booking> recentBookings = [];
 }
